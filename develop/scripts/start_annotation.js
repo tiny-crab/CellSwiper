dd76cb28620664afc7557f5c7c4c22329aee7d96
AetherUnbound <bowdenm@spu.edu>
2017-02-25 12:59:58 -0800

    Update README and add JS for start annotation form

---
+++ develop/scripts/start_annotation.js
@@ -0,0 +1,23 @@
+//Logic for starting the annotation process
+
+console.log("Start annotation script received");
+var name;
+var structure;
+
+$(document).ready( ()=> {
+	$("#start").click( ()=> {
+		if (!$("#name").val()) {
+			alert("Name field not filled");
+		}
+		else if (!$("#structure").val()) {
+			alert("Structure field not filled");
+		}
+		else {
+			alert("Form Filled");
+            name = $("#name").val();
+            structure = $("#structure").val();
+            window.open("annotation.html");
+		}
+	});
+});
+
