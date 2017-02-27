b80df27e596d673bc457d15fa31ac633034498f7
Evan Wiederspan <evan.wiederspan@gmail.com>
2017-02-27 00:08:38 +0000

    Download working yay

--- test.js
+++ test.js
@@ -61,16 +61,22 @@ app.get('/deny', function(req, res) {
 // Evan stuff
 app.get('/export', function(req, res) {
 	// ensure that the tmp directory exists
-	fs.mkdir('/tmp/csv', () => {
+	fs.mkdir('/tmp/csv', (err) => {
+		if (!err) {
+			// directory just created, set permissions
+			fs.chmodSync('/tmp/csv', '777', (err) => {
+				res.send("Error: please try again");
+			});
+		}
 		db.none(`copy annotation to '/tmp/csv/export.csv' DELIMITER ',' CSV HEADER`).then(() => {
 			res.download(`/tmp/csv/export.csv`, 'export.csv', (err) => {
 				if (err) {
-					res.send("Error: Please try again");
+					res.send("Error downloading: Please try again");
 				}
 			});
 		}).catch((err) => {
 			// should only happen when tmp gets wiped immediately after creating the csv folder
-			res.send("Error: Please try again");
+			res.send("Error writing: Please try again");
 		});
 	});
 });
