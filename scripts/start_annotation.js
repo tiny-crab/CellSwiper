63ee339effd11b363c75e759e715f646bc331d76
zakattak <zachary.jay.crabtree@gmail.com>
2017-03-04 15:06:06 -0800

    beautify files

--- scripts/start_annotation.js
+++ scripts/start_annotation.js
@@ -4,24 +4,24 @@ console.log("Start annotation script received");
 var name;
 var structure;
 
-$(document).ready( ()=> {
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    $("#sidebar").hide();
+$(document).ready(() => {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $("#sidebar").hide();
     }
 
-	$("form").submit( (e)=> {
+    $("form").submit((e) => {
         e.preventDefault();
-		if (!$("#name").val()) {
-			alert("Name field not filled");
-		}
-		else if (!$("#structure").val()) {
-			alert("Structure field not filled");
-		}
-		else {
-			console.log("Form Filled");
+        if (!$("#name").val()) {
+            alert("Name field not filled");
+        }
+        else if (!$("#structure").val()) {
+            alert("Structure field not filled");
+        }
+        else {
+            console.log("Form Filled");
             name = $("#name").val();
             structure = $("#structure").val();
             window.location.href = `annotation?index=0&name=${name}&structure=${structure}`;
-		}
-	});
+        }
+    });
 });
--- scripts/start_page.js
+++ scripts/start_page.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('input').blur(function() {
+    $('input').blur(function () {
 
-    // check if the input has any value (if we've typed into it)
-    if ($(this).val())
-      $(this).addClass('used');
-    else
-      $(this).removeClass('used');
-  });
+        // check if the input has any value (if we've typed into it)
+        if ($(this).val())
+            $(this).addClass('used');
+        else
+            $(this).removeClass('used');
+    });
 
 });
