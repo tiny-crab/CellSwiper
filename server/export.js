e58826ac140d9b813c81107e47d23d03a37fdfd5
Evan Wiederspan <evan@wiederspan.org>
2017-03-07 14:56:09 -0800

    Refacting and commenting export.js

--- server/export.js
+++ server/export.js
@@ -4,30 +4,42 @@ module.exports = function(fs, db) {
     // Author: Evan
     // Purpose: (currently) Export entire database
     // Parameters: (currently) None
-    // Actions: TODO - Evan document
     // Outputs: CSV to download
     module.export_csv = function(req, res) {
-        // ensure that the tmp directory exists
-        fs.mkdir('/tmp/csv', (err) => {
-            if (!err) {
-                // directory just created, set permissions
-                fs.chmodSync('/tmp/csv', '777', (err) => {
-                    res.send("Error: please try again");
-                });
-            }
-            db.none(`copy annotation to '/tmp/csv/export.csv' DELIMITER ',' CSV HEADER`).then(() => {
-                res.download(`/tmp/csv/export.csv`, 'export.csv', (err) => {
-                    if (err) {
-                        res.send("Error downloading: Please try again\n" + err);
-                    }
-                });
-            }).catch((err) => {
-                // should only happen when tmp gets wiped immediately after creating the csv folder
-                res.send("Error writing: Please try again");
+        // whole process done as a single promise
+        new Promise(function(resolve, reject) {
+            // ensure that the tmp directory exists
+            fs.mkdir('/tmp/csv', (err) => {
+                // create csv file, set write permissions for all
+                // first if it was just created
+                if (!err) {
+                    fs.chmod('/tmp/csv', '777', (err) => {
+                        if (err) {
+                            // this should not happen
+                            reject(["Error chaning permissions: Please try again", err]);
+                        }
+                        resolve();
+                    });
+                }
+                else {
+                    resolve();
+                }
             });
+        }) // run the command to output the table to a file
+        // replace 'annotation' with a select query to get certain rows instead of the whole thing
+        .then(db.none(`copy annotation to '/tmp/csv/export.csv' DELIMITER ',' CSV HEADER`))
+        .then(function() {
+            // download file to client
+            res.download(`/tmp/csv/export.csv`, 'export.csv', (err) => {
+                if (err) {
+                    throw ["Error downloading: Please try again\n", err]
+                }
+            });
+        })
+        .catch(function(err) {
+            // runs on reject() or throw, meant to catch errors
+            res.send(err[0] + err[1])
         });
     }
-
     return module;
 };
-
