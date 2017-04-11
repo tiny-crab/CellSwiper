510ad8366e213ee6c37744850573595e9ec2e243
Aetherunbound <bowdenm@spu.edu>
2017-04-10 23:31:39 -0700

    Tested and analyzed images Trends: Alpha or transparency on pngs is bad 4x4 minimum resolution for jpgs and 6x6 for pngs

---
+++ test/phashtest.js
@@ -0,0 +1,23 @@
+const phash = require('phash-image');
+const fs = require('fs');
+const path = require('path');
+
+// sample PNGs acquired from:
+//      http://www.schaik.com/pngsuite/
+//      http://www.fnordware.com/superpng/samples.html
+// note: images with 'x' preface are intentionally corrupted
+
+let dir = './pngtest/'
+fs.readdir(dir, (err, files) => {
+    if (err) {
+        console.log(err)
+    }
+    for (let f of files) {
+        phash(path.join(dir, f), true, (err, hash) => {
+            if (err) {
+                console.log(err);
+            }
+            else console.log("File: " + f + "\tHash: " + hash);
+        });
+    }
+});
