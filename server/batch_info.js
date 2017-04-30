75700c18605380e162c5227366e8f1dbf0be0753
Matthew Bowden <bowdenm@spu.edu>
2017-04-30 11:50:51 -0700

    All batch info sorts by date added

--- server/batch_info.js
+++ server/batch_info.js
@@ -6,7 +6,7 @@ module.exports = function(db) {
     let module = {};
 
     module.getBatchInfo = function(req, res) {
-        db.any("SELECT * FROM batches")
+        db.any("SELECT * FROM batches ORDER BY date_added")
             .then(batches => {
                 if (batches) {
                     res.send(batches);
