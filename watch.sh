5f59cc2a0ca55aaa17fcf95627b429ebb84271fb
Matthew Bowden <bowdenm@spu.edu>
2017-04-30 13:13:40 -0700

    Add path to foldername

--- watch.sh
+++ watch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find $1 -type d | python -c "import sys; import json; dirs = [d[:-1] for d in sys.stdin]; del dirs[0]; sys.stdout.write(json.dumps({'folders': dirs}) + '\n')" > "$2/scripts/folders.json"
+find $1 -type d | python -c "import sys; import json; dirs = [d[:-1] + '/' for d in sys.stdin]; del dirs[0]; sys.stdout.write(json.dumps({'folders': dirs}) + '\n')" > "$2/scripts/folders.json"
