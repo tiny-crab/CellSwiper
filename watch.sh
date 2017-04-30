7dbb3f24a3113e95c97a55a70e9db101f4f3b9e6
Matthew Bowden <bowdenm@spu.edu>
2017-04-30 12:46:14 -0700

    Update watch script to give it parameters for data dir and output loc

--- watch.sh
+++ watch.sh
@@ -1,1 +1,2 @@
-find . -type d | python -c "import sys; import json; dirs = [d[2:-1] + '/' for d in sys.stdin]; del dirs[0]; sys.stdout.write(json.dumps({'folders': dirs}))" > scripts/folders.json 
+#!/usr/bin/env bash
+find $1 -type d | python -c "import sys; import json; dirs = [d[2:-1] + '/' for d in sys.stdin]; del dirs[0]; sys.stdout.write(json.dumps({'folders': dirs}))" > "$2/scripts/folders.json"
