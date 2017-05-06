1f238971ca9654cc431600ceca8f3e78ea770368
Aetherunbound <bowdenm@spu.edu>
2017-05-05 17:45:52 -0700

    Add server message collapsable panel on error modal

--- scripts/errors.js
+++ scripts/errors.js
@@ -8,9 +8,21 @@ let modalDivContent = `
         <div class="modal-content">
             <div class="alert alert-danger modal-header" style="margin-bottom: 0;">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h3 class="modal-title alert-danger" id="myModalLabel">Something went wrong...</h3>
+                <h3 class="modal-title alert-danger">Something went wrong...</h3>
             </div>
-            <div class="modal-body text-center">
+            <div id="err-body" class="modal-body text-center lead">
+            </div>
+            <div id="err-panel" class="panel panel-warning" style="margin-bottom: 0;">
+                <div class="panel-heading">
+                    <h5 class="panel-title">
+                        <a data-toggle="collapse" href="#err-panel-content">Server Output</a>
+                    </h5>
+                </div>
+                <div id="err-panel-content" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <pre id="err-pre-text">Panel body</pre>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
@@ -29,11 +41,13 @@ let showModalError;
 //      - change its text to the error message
 //      - display the modal
 // Outpus: None
-showModalError = function(err) {
+showModalError = function(err_msg, server_output) {
     // let response = err.responseText;
     if ($('#err-modal').length === 0) {
         $('body').append(modalDivContent);
     }
-    $('#err-modal .modal-body').text(err);
+    $('#err-body').text(err_msg);
+    if (server_output) { $("#err-pre-text").text(server_output)}
+    else { $("#err-panel").hide()}
     $('#err-modal').modal('show');
 };
